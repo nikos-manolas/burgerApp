7e663675fc77a2edc79dad75e3120eaf2a88dfaf
Nikos <nikos.manolas@gmail.com>
2019-03-28 16:25:10 +0000

    commit after eject and using proptypes on functional component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
-import classes from './Layout.css';
+import classes from './Layout.module.css';
 
 const layout = ( props ) => (
 	<Aux>
