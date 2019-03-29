87d31c9f307043b930c83530586ae82b8b5ab1e1
Nikos <nikos.manolas@gmail.com>
2019-03-29 17:16:29 +0000

    shouldComponentUpdate on Modal so OrderSummary does not re-render each time

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
+
 const orderSummary = ({ingredients, purchaseCancelled, purchaseContinued, price}) => {
 	const ingredientSummary = Object.keys(ingredients)
 		.map(igKey => {
