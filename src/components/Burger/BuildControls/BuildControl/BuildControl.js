4768d016997efc1fd5d69b1cf65709fdb71c69f1
Nikos <nikos.manolas@gmail.com>
2019-03-29 11:39:15 +0000

    buildcontrols showing now

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './BuildControl.module.css';
+
+const buildControl = ({label}) => (
+	<div className = {classes.BuildControl}>
+		<div className = {classes.Label}>{ label }</div>
+		<button className = {classes.Less}>Less</button>
+		<button className = {classes.More}>More</button>
+	</div>
+)
+
+export default buildControl;
