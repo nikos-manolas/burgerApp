56755390b88f8ec97b7fa6d60e924a0e344321cd
Nikos <nikos.manolas@gmail.com>
2019-03-29 17:03:18 +0000

    drawer toggle button

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Toolbar.module.css';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
 	<header className={classes.Toolbar}>
-		<div>MENU</div>
+		<DrawerToggle clicked={props.drawerToggleClicked}/>
 		<div className={classes.Logo}>
 			<Logo/>
 		</div>
