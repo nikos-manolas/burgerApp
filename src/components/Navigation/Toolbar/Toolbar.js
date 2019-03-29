c8aaa6a7ac93d957f4bc44f328d49210378882ad
Nikos <nikos.manolas@gmail.com>
2019-03-29 16:31:15 +0000

    adding functionality for mobile devices

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,9 +6,11 @@ import classes from './Toolbar.module.css';
 const toolbar = (props) => (
 	<header className={classes.Toolbar}>
 		<div>MENU</div>
-		<Logo />
-		<nav>
-			<NavigationItems />
+		<div className={classes.Logo}>
+			<Logo/>
+		</div>
+		<nav className={classes.DesktopOnly}>
+			<NavigationItems/>
 		</nav>
 	</header>
 );
