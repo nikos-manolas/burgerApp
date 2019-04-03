3c70ded733e93e1964a05791c283332fbfb1fd20
Nikos <nikos.manolas@gmail.com>
2019-04-03 15:53:08 +0100

    added some initial testing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
 	// constructor(props) {
 	// 	super(props);
 	// 	this.state = {...}
---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,32 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+desribe('auth reducer', () => {
+	const initialState = {
+		token: null,
+		userId: null,
+		error: null,
+		loading: false,
+		authRedirect: '/'
+	};
+
+	it('should return the initial state', () => {
+		expect(reducer(undefined, {})).toEqual(initialState);
+	});
+
+	it('should store the token upon login', () => {	
+		expect(reducer(initialState, 
+			{	
+				type: actionTypes.AUTH_SUCCESS, 
+				idToken: 'some-Token', 
+				userId: 'some-UserId'
+			}
+		)).toEqual({
+			token: 'some-Token',
+			userId: 'some-UserId',
+			error: null,
+			loading: false ,
+			authRedirect: '/'
+		});
+	});
+})
