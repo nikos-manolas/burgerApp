90d029669b601494629db58cc65db05826431e4e
Nikos <nikos.manolas@gmail.com>
2019-04-03 14:57:42 +0100

    optimizations of the code, added lazy loading as asynccomponent

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.module.css';
+import { updateObject, checkValidity } from '../../../shared/utility';
 import * as orderActions from '../../../store/actions/index';
 
 class ContactData extends Component {
@@ -108,36 +109,14 @@ class ContactData extends Component {
 		this.props.onOrderBurger(order, this.props.token);
 	}
 
-	checkValidity = (value, rules) => {
-		let isValid = true;
-		if (!rules) {
-			return true;
-		}
-
-		if (rules.required) {
-			isValid = value.trim() !== '' && isValid;
-		}
-		
-		if (rules.minLength) {
-			isValid = value.length >= rules.minLength && isValid;
-		}
-
-		if (rules.maxLength) {
-			isValid = value.length <= rules.maxLength && isValid;
-		}
-
-		return isValid;
-	}
-
 	inputChangedHandler = (event, inputIdentifier) => {
-		const updatedOrderForm = {
-			...this.state.orderForm
-		};
-		const updatedFormElement = {...updatedOrderForm[inputIdentifier]};
-		updatedFormElement.value = event.target.value;
-		updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation )
-		updatedFormElement.touched = true;
-		updatedOrderForm[inputIdentifier] = updatedFormElement;
+		const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+			value: event.target.value,
+			valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation ),
+			touched: true
+		});
+
+		const updatedOrderForm = updateObject(this.state.orderForm, {[inputIdentifier]: updatedFormElement});
 		
 		let formIsValid = true;
 		for (let inputIdentifierz in updatedOrderForm) {
